aee6dea31a1fa6e9419760e51f18d99f8ae648ce
Mario IC <mario21ic@gmail.com>
2022-11-17 01:40:00 -0500

    updating

--- vars/dockerLib.groovy
+++ vars/dockerLib.groovy
@@ -1,10 +1,15 @@
-def build(Map params) {
-    //sh "docker build -f $params.DockerfilePath} -t ${params.DockerImage} ${params.DockerContext}"
-    echo "docker build -f $params.DockerfilePath} -t ${params.DockerImage} ${params.DockerContext}"
+def build(Map params){
+    //sh "docker build -f ${params.DockerfilePath} -t ${params.DockerImage} ${params.DockerArgs} ${params.DockerContext}"
+    echo "docker build -f ${params.DockerfilePath} -t ${params.DockerImage} ${params.DockerArgs} ${params.DockerContext}"
 }
 
-def push(Map params) {
+def push(Map params){
     //sh "docker push ${params.DockerImage}"
     echo "docker push ${params.DockerImage}"
 }
 
+def promoter(Map params){
+    sh "docker pull ${params.DockerImage}"
+    sh "docker tag ${params.DockerImage} ${params.DockerNewImage}"
+    sh "docker push ${params.DockerNewImage}"
+}
