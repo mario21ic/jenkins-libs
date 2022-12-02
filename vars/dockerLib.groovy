5d39ce2ba0a1f9a534137952086984d22f4bc750
Mario IC <mario21ic@gmail.com>
2022-12-01 21:39:39 -0500

    updating env vars

--- vars/dockerLib.groovy
+++ vars/dockerLib.groovy
@@ -1,11 +1,14 @@
 def build(Map params){
     echo "myvar: ${this.env.MYVAR}"
+    echo "${MYVAR}"
     //sh "docker build -f ${params.DockerfilePath} -t ${params.DockerImage} ${params.DockerArgs} ${params.DockerContext}"
     echo "docker build -f ${params.DockerfilePath} -t ${params.DockerImage} ${params.DockerArgs} ${params.DockerContext}"
 }
 
 def push(Map params){
     //sh "docker push ${params.DockerImage}"
+    echo "myvar: ${this.env.MYVAR}"
+    echo "${MYVAR}"
     echo "docker push ${params.DockerImage}"
 }
 
