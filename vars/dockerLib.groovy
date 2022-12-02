1e84f87448ee3b44889390132616f2f7961339b7
Mario IC <mario21ic@gmail.com>
2022-12-01 21:53:48 -0500

    fixing dockerLib

--- vars/dockerLib.groovy
+++ vars/dockerLib.groovy
@@ -1,12 +1,12 @@
 def build(Map params){
-    //echo "myvar: ${this.env.MYVAR}"
+    echo "myvar: ${this.env.MYVAR}"
     //sh "docker build -f ${params.DockerfilePath} -t ${params.DockerImage} ${params.DockerArgs} ${params.DockerContext}"
     echo "docker build -f ${params.DockerfilePath} -t ${params.DockerImage} ${params.DockerArgs} ${params.DockerContext}"
 }
 
 def push(Map params){
+    echo "myvar: ${this.env.MYVAR}"
     //sh "docker push ${params.DockerImage}"
-    //echo "myvar: ${this.env.MYVAR}"
     echo "docker push ${params.DockerImage}"
 }
 
